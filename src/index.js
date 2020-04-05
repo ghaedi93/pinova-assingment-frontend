c233c8a561b1e4606da5e9529da34de0d986ed8c
Javad Ghaedi <Ghaedi1993@gmail.com>
2020-04-05 21:13:57 +0430

    moved store to a seprate file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { createStore, applyMiddleware } from "redux";
+// import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-import thunk from "redux-thunk";
-import rootReducer from "./reducers";
-const store = createStore(rootReducer, applyMiddleware(thunk));
-
+// import thunk from "redux-thunk";
+// import rootReducer from "./reducers";
+// const store = createStore(rootReducer, applyMiddleware(thunk));
+import store from "./store";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
