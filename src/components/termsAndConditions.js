a8148de8cb114800f5f89c34293486ad9a056ffb
Javad Ghaedi <Ghaedi1993@gmail.com>
2020-04-05 10:18:10 +0430

    terms and welcome handlers added

--- src/components/termsAndConditions.js
+++ src/components/termsAndConditions.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
 
 class Terms extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      termsAndConditions: "",
+    };
+  }
+  countiue = () => {
+    this.setState({
+      ...this.state,
+      termsAndConditions: true,
+    });
+  };
   render() {
     return (
       <div>
-        <p>terms and conditions are this and that i hope you are agree</p>
-        <button>continue</button>
+        <p>terms and conditions are this and that i hope you are agreed</p>
+        <button onClick={this.countiue}>continue</button>
       </div>
     );
   }
--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
 
 class Welcome extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      welcomeScreen: "",
+    };
+  }
+  continue = () => {
+    this.setState({
+      ...this.state,
+      welcomeScreen: true,
+    });
+  };
   render() {
     return (
       <div>
         <h1>Welcome ...</h1>
         <p>Its greet to have you here your balance is ...</p>
-        <button>continue</button>
+        <button onClick={this.continue}>continue</button>
       </div>
     );
   }
