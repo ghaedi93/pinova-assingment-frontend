007516c63a93ea0eb59bf894dc311bcabf638499
Javad Ghaedi <Ghaedi1993@gmail.com>
2020-04-05 10:06:06 +0430

    adding handlers for user info

--- src/components/userInformation.js
+++ src/components/userInformation.js
@@ -1,35 +1,78 @@
 import React, { Component } from "react";
 
 class UserInformation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      countryCode: "",
+      phoneNumber: "",
+      acceptMarketing: false,
+    };
+  }
+  setEmail = (e) => {
+    const email = e.target.value;
+    this.setState({
+      ...this.state,
+      email,
+    });
+  };
+  setCountryCode = (e) => {
+    const countryCode = e.target.value;
+    this.setState({
+      ...this.state,
+      countryCode,
+    });
+  };
+  setPhoneNumber = (e) => {
+    const phoneNumber = e.target.value;
+    this.setState({
+      ...this.state,
+      phoneNumber,
+    });
+  };
+  setAcceptMarketing = (e) => {
+    const acceptMarketing = e.target.checked;
+    this.setState({
+      ...this.state,
+      acceptMarketing,
+    });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  };
   render() {
     return (
-      <form
-      //    onSubmit={this.handleSubmit}
-      >
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label htmlFor="title">email</label>
           <input
             type="text"
             id="email"
             // value={userName}
-            // onChange={this.handleChange}
+            onChange={this.setEmail}
           />
           <label htmlFor="title">countryCode</label>
           <input
             type="text"
             id="countryCode"
             // value={password}
-            // onChange={this.handleChange}
+            onChange={this.setCountryCode}
           />
           <label htmlFor="title">phoneNumber</label>
           <input
             type="text"
             id="phoneNumber"
             // value={password}
-            // onChange={this.handleChange}
+            onChange={this.setPhoneNumber}
           />
           <p>do you agree to getting ads ?</p>
-          <input type="checkbox" id="acceptMarketing" />
+          <input
+            type="checkbox"
+            id="acceptMarketing"
+            onChange={this.setAcceptMarketing}
+          />
         </div>
         <button type="submit">continue</button>
       </form>
