2acdd6aa23bfa3a63345f8bd3f92c1a9d877ecda
Javad Ghaedi <Ghaedi1993@gmail.com>
2020-04-05 09:44:18 +0430

    adding app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import Login from "./components/login-registration";
+import { Route, Switch } from "react-router-dom";
+import UserInformation from "./components/userInformation";
+import Terms from "./components/termsAndConditions";
+import Welcome from "./components/welcome";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <main>
+        <Switch>
+          <Route path="/" component={Login} exact />
+          <Route path="/userInformation" component={UserInformation} />
+          <Route path="/terms" component={Terms} />
+          <Route path="/welcome" component={Welcome} />
+        </Switch>
+      </main>
+    </div>
+  );
 }
 
 export default App;
